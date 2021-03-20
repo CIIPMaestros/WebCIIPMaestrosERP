71b44818590e4771a734703dfc7ab76911792de0
SantiiBb <santii_mejia@live.com>
2021-03-20 03:04:24 -0600

    modelo bueno

--- WebCIIPMaestrosERP/Models/DB_CIIPMaestrosERP.Context.cs
+++ WebCIIPMaestrosERP/Models/DB_CIIPMaestrosERP.Context.cs
@@ -43,6 +43,7 @@ namespace WebCIIPMaestrosERP.Models
         public virtual DbSet<MAE_CURSOS> MAE_CURSOS { get; set; }
         public virtual DbSet<MAE_CURSOS_LANZAMIENTOS> MAE_CURSOS_LANZAMIENTOS { get; set; }
         public virtual DbSet<ViewMKTDocenteCurso> ViewMKTDocenteCurso { get; set; }
+        public virtual DbSet<MAE_CURSOS_HORARIOS_FUTURO> MAE_CURSOS_HORARIOS_FUTURO { get; set; }
     
         public virtual ObjectResult<Nullable<System.DateTime>> Sp_Crear_Lanzamientos_Masivo(Nullable<int> vAR1, Nullable<int> vAR2)
         {
